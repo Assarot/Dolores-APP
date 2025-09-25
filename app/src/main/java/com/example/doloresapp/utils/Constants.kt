ffc5dd39bcc21125b114affc6ba1d1ae9347f890
kat14221 <ale123ferkatty@gmail.com>
2025-09-25 09:44:38 -0500

    editandocolore

--- app/src/main/java/com/example/doloresapp/utils/Constants.kt
+++ app/src/main/java/com/example/doloresapp/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.doloresapp.utils
 
 object Constants {
-    const val BASE_URL: String = "http://10.0.2.2:8090/api/"
+    const val BASE_URL: String = "http://10.40.29.214:8090/api/"
 }
